9095d947f913356a04e7913b220c10479a98f61a
Andrés Sneer <andresduranrod@gmail.com>
2019-05-07 15:19:56 +0000

    add read_write_SD

---
+++ read_write_SD/read_write_SD.ino
@@ -0,0 +1,75 @@
+
+#include <SPI.h>
+#include <SD.h>
+
+int contador=0;
+
+File myFile;
+
+void setup() {
+  // Open serial communications and wait for port to open:
+  Serial.begin(9600);
+  while (!Serial) {
+    ; // wait for serial port to connect. Needed for native USB port only
+  }
+
+
+  Serial.print("Initializing SD card...");
+
+  if (!SD.begin(4)) {
+    Serial.println("initialization failed!");
+    return;
+  }
+  Serial.println("initialization done.");
+
+  // open the file. note that only one file can be open at a time,
+  // so you have to close this one before opening another.
+  myFile = SD.open("test.txt", FILE_WRITE);
+
+  // if the file opened okay, write to it:
+  if (myFile) {
+    Serial.print("Writing to test.txt...");
+    myFile.println("testing 1, 2, 3.");
+    // close the file:
+    myFile.close();
+    Serial.println("done.");
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+
+  // re-open the file for reading:
+  myFile = SD.open("test.txt");
+  if (myFile) {
+    Serial.println("test.txt:");
+
+    // read from the file until there's nothing else in it:
+    while (myFile.available()) {
+      Serial.write(myFile.read());
+    }
+    // close the file:
+    myFile.close();
+  } else {
+    // if the file didn't open, print an error:
+    Serial.println("error opening test.txt");
+  }
+}
+
+void loop() {
+  if (contador<10)
+  {
+    myFile = SD.open("test.txt", FILE_WRITE);
+    if (myFile) {
+      Serial.print("Count = ");
+      Serial.print(contador);
+      myFile.print("Contador: ");
+      myFile.println(contador);
+      myFile.close();
+      Serial.println(" Done.");
+    } else {
+      Serial.print("error con contador ");
+      Serial.println(contador);
+    }
+    contador=contador+1;
+  }
+}
